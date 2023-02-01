1a8cb99a50ddd8858353c178c68188e0de50743a
興怡 <>
2023-02-01 01:41:22 -0800

    try redis next

--- components/skeleton-zig/build.zig
+++ components/skeleton-zig/build.zig
@@ -2,33 +2,32 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
     b.setPreferredReleaseMode(std.builtin.Mode.ReleaseSafe);
-    const xt = try std.zig.CrossTarget.parse(.{.arch_os_abi = "wasm32-wasi"});
-    const target = b.standardTargetOptions(.{.default_target = xt});
+    const wasi = try std.zig.CrossTarget.parse(.{.arch_os_abi = "wasm32-wasi"});
+    const target = b.standardTargetOptions(.{.default_target = wasi});
 
-    const lib = b.addStaticLibrary("fermyon", null);
+    const lib = b.addStaticLibrary("spin", null);
     lib.setTarget(target);
     lib.linkLibC();
-    lib.addIncludeDir("deps/fermyon/http");
-    lib.addIncludeDir("deps/fermyon/redis");
+    lib.addIncludeDir("deps/spin/http");
+    lib.addIncludeDir("deps/spin/redis");
     lib.addCSourceFiles(&.{
-        "deps/fermyon/http/spin-http.c",
-        "deps/fermyon/http/wasi-outbound-http.c",
-        "deps/fermyon/redis/spin-redis.c",
-        "deps/fermyon/redis/outbound-redis.c",
+        "deps/spin/http/spin-http.c",
+        "deps/spin/http/wasi-outbound-http.c",
+        "deps/spin/redis/spin-redis.c",
+        "deps/spin/redis/outbound-redis.c",
     }, &.{
         "-Wall",
         "-Wno-unused-parameter",
         "-Wno-switch-bool",
     });
 
-
     const exe = b.addExecutable("webcomponent", "src/component.zig");
     exe.setTarget(target);
     exe.install();
     exe.linkLibC();
     exe.linkLibrary(lib);
-    exe.addIncludeDir("deps/fermyon/http");
-    exe.addIncludeDir("deps/fermyon/redis");
+    exe.addIncludeDir("deps/spin/http");
+    exe.addIncludeDir("deps/spin/redis");
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
--- components/skeleton-zig/src/component.zig
+++ components/skeleton-zig/src/component.zig
@@ -1,28 +1,80 @@
 const std = @import("std");
-const sp = @cImport({
+const sdk = @cImport({
     @cInclude("stddef.h");
     @cInclude("spin-http.h");
 });
 
-// To be invoked by Spin executor
-export fn spin_http_handle_http_request(req: *sp.spin_http_request_t, res: *sp.spin_http_response_t) callconv(.C) void {
-    defer sp.spin_http_request_free(req);
+// Entry point required by the Spin host
+export fn spin_http_handle_http_request(req: *sdk.spin_http_request_t, res: *sdk.spin_http_response_t) callconv(.C) void {
+    defer sdk.spin_http_request_free(req);
 
-    if (req.method != sp.SPIN_HTTP_METHOD_GET) {
-        res.status = @as(c_uint, 418);
-        ////res.status = sp.SPIN_HTTP_HTTP_ERROR_REQUEST_ERROR;
+    // TODO restrict path? (or just rely on routing)
+
+    if (req.method != sdk.SPIN_HTTP_METHOD_POST) {
+        res.status = @as(c_uint, 405);
         return;
     }
 
+    // TODO Verify header for Signature and Content-Type
 
-    // TODO
+    const sz = req.body.val.len;
+    if (req.body.is_some) {
+        if (sz > 1048576) {
+            // restrict max to 1MB
+            res.status = @as(c_uint, 413);
+            return;
+        }
+
+        const bodslc = req.body.val.ptr[0..sz];
+        std.testing.expectEqual(bodslc.len, sz) catch {
+            //TODO is this un/safe? (slicing c-ptr)
+            res.status = @as(c_uint, 422);
+            return;
+        };
+
+        const wellformed = std.json.validate(bodslc);
+        if (!wellformed) {
+            res.status = @as(c_uint, 406);
+            return;
+        }
+    }
+
+    // rehydrate json and dispatch by activity-type
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit());
+    const allocator = gpa.allocator();
+    var parser = std.json.Parser.init(allocator, false);
+    var tree = parser.parse(req.body.val.ptr[0..sz]) catch {
+        res.status = @as(c_uint, 417);
+        return;
+    };
+    defer tree.deinit();
+    const found = tree.root.Object.contains("type");
+    if (!found) {
+        res.status = @as(c_uint, 424);
+        return;
+    }
+    var elem = tree.root.Object.get("type").?;
+    switch (elem) {
+        //"Reject", "Undo" => res.status = @as(c_uint, 418),
+        //"Accept" => res.status = @as(c_uint, 418),
+        //"Follow" => res.status = @as(c_uint, 418),
+        .String => |val| {
+            res.status = @as(c_uint, 207);
+            //TODO publish to redis
+            _ = val;
+            return;
+        },
+        else => {res.status = @as(c_uint, 418); return;},
+    }
+
     res.status = @as(c_uint, 200);
-    ////res.status = sp.SPIN_HTTP_HTTP_ERROR_SUCCESS;
 }
 
 
 
+// Stub needed to suppress a "import env::main" error
 pub fn main() void {
-    std.debug.print("main function entry", .{});
+    std.debug.print("main function stub", .{});
 }
 
