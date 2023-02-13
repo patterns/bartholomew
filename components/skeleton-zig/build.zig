c81b741c58a3e052ffcbf3f3d38c74b2f2fb3b89
興怡 <>
2023-02-13 02:26:52 -0800

    delink libc pt1

--- components/skeleton-zig/build.zig
+++ components/skeleton-zig/build.zig
@@ -1,51 +1,23 @@
 const std = @import("std");
-const cow_list = std.build.Pkg{ .name = "cow_list", .source = .{ .path = "libs/zigstr/libs/cow_list/src/main.zig" } };
-const ziglyph = std.build.Pkg{ .name = "ziglyph", .source = .{ .path = "libs/zigstr/libs/ziglyph/src/ziglyph.zig" } };
-const Zigstr = std.build.Pkg{
-    .name = "Zigstr",
-    .source = .{ .path = "libs/zigstr/src/Zigstr.zig" },
-    .dependencies = &[_]std.build.Pkg{ cow_list, ziglyph },
-};
-
 
 pub fn build(b: *std.build.Builder) !void {
     const ww = try std.zig.CrossTarget.parse(.{.arch_os_abi = "wasm32-wasi"});
     const target = b.standardTargetOptions(.{.default_target = ww});
-
-    const lib = b.addStaticLibrary("spin", null);
-    lib.setBuildMode(std.builtin.Mode.ReleaseSmall);
-    lib.setTarget(target);
-    lib.linkLibC();
-    lib.addIncludePath("libs/spin/http");
-    lib.addIncludePath("libs/spin/redis");
-    lib.addCSourceFiles(&.{
-        "libs/spin/http/spin-http.c",
-        "libs/spin/http/wasi-outbound-http.c",
-        "libs/spin/redis/outbound-redis.c",
-        ////"libs/spin/redis/spin-redis.c",
-    }, &.{
-        "-Wall",
-        "-Wno-unused-parameter",
-        "-Wno-switch-bool",
-    });
+    //const mode = b.standardReleaseOptions();
+    const mode = std.builtin.Mode.ReleaseSmall;
 
     const exe = b.addExecutable("webcomponent", "src/main.zig");
-    exe.setBuildMode(std.builtin.Mode.ReleaseSafe);
+    exe.setBuildMode(mode);
     exe.setTarget(target);
     exe.install();
-    exe.linkLibC();
-    exe.linkLibrary(lib);
-    exe.addPackage(Zigstr);
-    exe.addIncludePath("libs/spin/http");
-    exe.addIncludePath("libs/spin/redis");
+    exe.single_threaded = true;
 
+    // export symbols (required in 0.11 see zig/issues/14139)
+    const export_names = [_][]const u8 {
+        "canonical_abi_free",
+        "canonical_abi_realloc",
+        "handle-http-request",
+    };
+    exe.export_symbol_names = &export_names;
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }
--- components/skeleton-zig/src/main.zig
+++ components/skeleton-zig/src/main.zig
@@ -1,75 +1,13 @@
 const std = @import("std");
-const sdk = @cImport({
-    @cInclude("stddef.h");
-    @cInclude("spin-http.h");
-});
-const str = @import("strings.zig");
-const store = @import("store.zig");
-const status = @import("status.zig");
-const sign = @import("signature.zig");
-const Gpa = std.heap.GeneralPurposeAllocator(.{});
 
-// Entry point required by the Spin host.
-pub export fn spin_http_handle_http_request(
-    req: *sdk.spin_http_request_t,
-    res: *sdk.spin_http_response_t,
-) callconv(.C) void {
-    defer sdk.spin_http_request_free(req);
+const sdk = @import("wasm.zig");
 
-    // For an inbox endpoint, only allow POST requests.
-    if (req.method != sdk.SPIN_HTTP_METHOD_POST) return status.nomethod(res);
-    if (!req.body.is_some) return status.bad(res);
-
-    var gpa = Gpa{};
-    defer std.debug.assert(!gpa.deinit());
-    const gpal = gpa.allocator();
-
-    var headers = str.pairs(gpal, req);
-    defer headers.deinit();
-    // TODO free the k/v pairs
-    //DEBUG
-    const sig_check = sign.verify(gpal, headers);
-    if (!str.eq("SIGN.FAIL", sig_check)) {
-        return status.json(res);
-    } else {
-        return status.forbidden(res);
-    }
-
-    const sz = req.body.val.len;
-    // restrict max to 1MB
-    if (sz > 1048576) return status.toolarge(res);
-
-    var json_raw = std.ArrayList(u8).init(gpal);
-    defer json_raw.deinit();
-    json_raw.appendSlice(req.body.val.ptr[0..sz]) catch return status.noaccept(res);
-    var tree = str.toTree(gpal, json_raw) catch return status.unprocessable(res);
-    defer tree.deinit();
-
-    const activity_found = tree.root.Object.contains("type");
-    if (!activity_found) return status.precondition(res);
-
-    // JSON value for the (activity) "type" property must be of string.
-    var json_val = tree.root.Object.get("type").?;
-    const act = json_val.String;
-    if (str.eq("Reject", act) or str.eq("Undo", act)) {
-        store.enqueue(gpal, tree) catch return status.storage(res);
-    } else if (str.eq("Accept", act)) {
-        store.enqueue(gpal, tree) catch return status.storage(res);
-    } else if (str.eq("Follow", act)) {
-        store.enqueue(gpal, tree) catch return status.storage(res);
-    } else {
-        store.alarm(gpal, &json_raw, headers) catch return status.unavailable(res);
-        return status.nocontent(res);
-    }
-
-    status.ok(res);
-}
-
-// Stub needed to suppress a "import env::main" error.
 pub fn main() void {
     std.debug.print("main function stub", .{});
 }
 
-//const ConfigError = error{RedisAddress};
-//const LimitError = error{BufferSize};
-
+comptime {
+    @export(sdk.HttpHandler, .{ .name = "handle-http-request", .linkage = .Strong });
+    @export(sdk.CanonicalAbiRealloc, .{ .name = "canonical_abi_realloc", .linkage = .Strong });
+    @export(sdk.CanonicalAbiFree, .{ .name = "canonical_abi_free", .linkage = .Strong });
+}
